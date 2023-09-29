69d12fe409179e7665dbd5dc773b334111c3f75b
Daniel Barbosa <danielolibar@gmail.com>
2023-09-29 16:26:26 -0300

    general code cleanup and commenting

--- models/models.py
+++ models/models.py
@@ -6,7 +6,7 @@ class Point:
     self.x = x
     self.y = y
   
-  # Sobrecarga do operador de subtração
+  # Overload of the subtraction operator
   def __sub__(self, other):
     return Point(self.x - other.x, self.y - other.y)
 
@@ -17,18 +17,22 @@ class Segment:
       self.origin = _origin
       self.destination = _destination
 
-  # Positivo -> self está no sentido horário em relação a point
-  # Negativo -> self está no sentido anti-horário em relação a point
-  # Zero -> Colineares
+
+  # Positive -> self is clockwise in relation to point
+  # Negative -> self is counter-clockwise in relation to point
+  # Zero -> Colinear
   def direction(self, point):
     
-    # Define os pontos e toma p0 como origem do plano cartesiano
+    # Defines the points and takes p0 as the 
+    # origin of the cartesian plane
     p0 = self.origin
     p1 = self.destination - p0
     p2 = point - p0
     
     return p1.x * p2.y - p1.y * p2.x
 
+
+  # Returns true if the point is on the segment
   def pointOnSegment(self, point):
 
     pointInXInterval = (point.x >= min(self.origin.x, self.destination.x) and point.x <= max(self.origin.x, self.destination.x))
@@ -38,6 +42,8 @@ class Segment:
       return True
     return False
 
+
+  # Returns true if the segments intersect
   def segmentIntersect(self, segmentB):
 
     d1 = segmentB.direction(self.origin)
@@ -45,8 +51,11 @@ class Segment:
     d3 = self.direction(segmentB.origin)
     d4 = self.direction(segmentB.destination)
 
+    # General cases
     if ((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0)):
       return True
+    
+    # Colinear cases
     elif d1 == 0 and segmentB.pointOnSegment(self.origin):
       return True
     elif d2 == 0 and segmentB.pointOnSegment(self.destination):
@@ -58,22 +67,51 @@ class Segment:
     else:
       return False
     
-  def leftmostPoint(self):
-      if self.origin.x < self.destination.x or (self.origin.x == self.destination.x and self.origin.y < self.destination.y):
-          return self.origin
-      else:
-          return self.destination
 
+  # Returns the leftmost point of the segment
+  def leftmostPoint(self):
+    
+    originIsLeftmost = self.origin.x < self.destination.x
+    pointsHaveSameX = self.origin.x == self.destination.x
+    originIsLowest = self.origin.y < self.destination.y
+
+    if originIsLeftmost or (pointsHaveSameX and originIsLowest):
+        return self.origin
+    else:
+        return self.destination
+
+
+  # Returns if a segment is less than another segment
   def __lt__(self, other):
+      
       selfLeftmost = self.leftmostPoint()
       otherLeftmost = other.leftmostPoint()
+      
+      # A segment A is less than another segment B 
+      # if the leftmost point of A is to the left of 
+      # the leftmost point of B.
       if selfLeftmost.x < otherLeftmost.x:
-          return True
+        return True
+      
+      # If both segments have the same leftmost x 
+      # coordinates, the segment with the lowest y 
+      # coordinate is less than the other.
       elif selfLeftmost.x == otherLeftmost.x:
-          return selfLeftmost.y < otherLeftmost.y
+        return selfLeftmost.y < otherLeftmost.y
+      
       return False
 
+
+  # Two points are equal if they have the same 
+  # leftmost point
   def __eq__(self, other):
-      selfLeftmost = self.leftmostPoint()
-      otherLeftmost = other.leftmostPoint()
-      return selfLeftmost.x == otherLeftmost.x and selfLeftmost.y == otherLeftmost.y
+    
+    selfLeftmost = self.leftmostPoint()
+    otherLeftmost = other.leftmostPoint()
+
+    segmentsLeftmostXCoordSame = selfLeftmost.x == otherLeftmost.x
+    segmentsLeftmostYCoordSame = selfLeftmost.y == otherLeftmost.y
+
+    segmentsHaveSameLeftmostPoint = segmentsLeftmostXCoordSame and segmentsLeftmostYCoordSame
+    
+    return segmentsHaveSameLeftmostPoint
